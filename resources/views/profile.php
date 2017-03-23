594f97cb6fb825d97a3e0622501420f19c4dfeee
BassemHermina <bassems.design@gmail.com>
2017-03-23 11:38:54 +0200

    normal workflow edits

--- resources/views/profile.php
+++ resources/views/profile.php
@@ -5,6 +5,8 @@
         <h1>Hello, {{ $name }}</h1>
 
         <br>
-		<h3><a href='http://41.43.53.38:8000/updateserver'>- Update Server</a></h3>
+		<h3>
+			<a href="http://41.43.53.38:8000/updateserver">- Update Server</a>
+		</h3>
     </body>
 </html>
